7fb4e61c605a508b4a45b45f35f8236789537c6b
octacian <enduffy2014@outlook.com>
2020-02-26 22:17:52 -0800

    Rewrite and fix coverage statistic filter

--- run.lua
+++ run.lua
@@ -8,8 +8,8 @@
 
 local STATS_FILE_NAME = "luacov.stats.out"
 local REPORT_FILE_NAME = "luacov.report.out"
-local DEBUG = os.getenv("DEBUG") == "true" and true or false
 
+local DEBUG = os.getenv("DEBUG") == "true" and true or false
 local function print_debug(...)
 	if DEBUG then print(...) end
 end
@@ -17,6 +17,11 @@ end
 local function exec(cmd)
 	print("> " .. cmd)
 	os.execute(cmd)
+end
+
+local function ends_with(str, ending)
+	print_debug(str .. " ends with " .. ending .. "? Ending: " .. str:sub(-(#ending + 4), -5))
+	return str:sub(-(#ending + 4), -5) == ending
 end
 
 os.remove(STATS_FILE_NAME)
@@ -33,23 +38,19 @@ exec(("busted --lua=lua5.1 --coverage %s ."):format(pattern))
 
 local file = io.open(STATS_FILE_NAME, "r")
 local lines = {}
-local ignore_line = 0
+local retain = 0
 for line in file:lines() do
-	if ignore_line == 1 then
-		print_debug(tostring(ignore_line) .. "- "  .. " " .. line)
-		ignore_line = 0
-	elseif line:find("/usr/") or line:find("_spec") or (ignore_line == 0 and filter ~= "" and not line:find(filter)) then
-		ignore_line = 1
-		print_debug(tostring(ignore_line) .. "! "  .. " " .. line)
-	else
-		print_debug(tostring(ignore_line) .. "+ "  .. " " .. line)
+	-- if line is a header and the entry should be retained, retain 2 lines (this and the next)
+	if line:find(".lua") and not (line:find("/usr/") or line:find("_spec")) and (filter == "" or ends_with(line, filter)) then
+		retain = 2
+	end
+
+	-- if a line should be retained, insert it into lines and decrement retainer
+	if retain > 0 then
 		table.insert(lines, line)
-		if ignore_line == -1 then
-			if line:find(":") then ignore_line = 0 end
-		else ignore_line = -1 end
+		retain = retain - 1
 	end
 end
-file:close()
 
 file = io.open(STATS_FILE_NAME, "w")
 for _, line in ipairs(lines) do

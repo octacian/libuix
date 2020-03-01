dfc0d568f4c2770f0e4a4ec8e9aa8d8454e397e6
octacian <enduffy2014@outlook.com>
2020-03-01 11:31:51 -0800

    Expand Manager class unit tests and enforce arguments

--- formspec/manager.lua
+++ formspec/manager.lua
@@ -1,4 +1,5 @@
--- local static_table = dofile(modpath.."/utility.lua").static_table
+local utility = dofile(modpath.."/utility.lua")
+
 local Model = dofile(modpath.."/formspec/model.lua")
 local Elements = dofile(modpath.."/formspec/elements.lua")
 setmetatable(Elements, { __index = _G })
@@ -12,16 +13,17 @@ FormspecManager.__index = FormspecManager
 
 -- Creates a new FormspecManager instance.
 function FormspecManager:new(modname)
+	utility.enforce_types({"string"}, modname)
 	local instance = { modname = modname, forms = {} }
 	setmetatable(instance, FormspecManager)
-	return instance --static_table(instance, FormspecManager)
+	return instance
 end
 
 -- Collects formspec name, elements, and model for addition to the instance.
 function FormspecManager:__call(name)
+	utility.enforce_types({"string"}, name)
 	-- Add element functions to the global environment.
 	setfenv(2, Elements)
-	-- print(dump(self))
 
 	-- Accept options, including size and the `fixed_size` and `no_prepend` controls.
 	return function(options)
@@ -29,6 +31,7 @@ function FormspecManager:__call(name)
 		return function(elements)
 			-- Accept data model table.
 			return function(model)
+				utility.enforce_types({"table", "table", "table"}, options, elements, model)
 				setfenv(2, getmetatable(Elements).__index) -- Remove Elements from the global environment.
 				self:add(name, options, elements, model)
 			end
@@ -38,10 +41,12 @@ end
 
 -- Adds a formspec to the instance.
 function FormspecManager:add(name, options, elements, model)
-	assert(not self:get_index(name), ("libuix().formspec: '%s' already exists"):format(name))
-	assert(type(options) == "table", ("libuix().formspec(%s): options must be a table"):format(name))
-	assert(type(options.w) == "number", ("libuix().formspec(%s): options.w must be a number"):format(name))
-	assert(type(options.h) == "number", ("libuix().formspec(%s): options.h must be a number"):format(name))
+	utility.enforce_types({"string", "table", "table", "table"}, name, options, elements, model)
+	assert(not self:get_index(name), ("libuix().formspec: form '%s' already exists"):format(name))
+	table.constrain(options, {
+		{"w", "number"},
+		{"h", "number"}
+	})
 
 	table.insert(self.forms, {
 		name = name,
@@ -53,6 +58,7 @@ end
 
 -- Gets a formspec by name from the instance.
 function FormspecManager:get(name)
+	utility.enforce_types({"string"}, name)
 	for _, form in ipairs(self.forms) do
 		if form.name == name then
 			return form
@@ -62,6 +68,7 @@ end
 
 -- Gets a formspec index by name from the instance.
 function FormspecManager:get_index(name)
+	utility.enforce_types({"string"}, name)
 	for index, form in ipairs(self.forms) do
 		if form.name == name then
 			return index
@@ -72,10 +79,13 @@ end
 -- Renders a formspec identified by name, returning a Minetest-compatible
 -- formspec string unless the name refers to a non-existent formspec.
 function FormspecManager:render(name, real_coordinates)
+	utility.enforce_types({"string", "boolean?"}, name, real_coordinates)
+
 	local form = self:get(name)
 	if not form then return end
 
 	local formstring = ""
+	-- TODO: Move `real_coordinates` control to formspec options
 	if real_coordinates ~= false then
 		formstring = "real_coordinates[true]"
 	end
@@ -95,8 +105,10 @@ end
 
 -- Shows a formspec to a player, both identified by name.
 function FormspecManager:show(form_name, player_name)
+	utility.enforce_types({"string", "string"}, form_name, player_name)
+
 	local formstring = self:render(form_name)
-	assert(formstring, ("libuix!FormspecManager:show: '%s' does not exist"):format(form_name))
+	assert(formstring, ("libuix().formspec['%s']:show: formspec does not exist or contains no elements"):format(form_name))
 
 	minetest.show_formspec(player_name, self.modname .. ":" .. form_name, formstring)
 end
--- tests/formspec/manager_spec.lua
+++ tests/formspec/manager_spec.lua
@@ -1,22 +1,78 @@
 package.path = "../?.lua;" .. package.path
 _G.libuix = {}
 _G.modpath = "."
+_G.minetest = {}
 local FormspecManager = require("formspec/manager")
 
--- TODO: Test that formspec options are all correctly handled.
+-- show_formspec mock function
+local last_show_formspec_call = {}
+function minetest.show_formspec(player_name, form_name, formstring)
+	last_show_formspec_call = {player_name, form_name, formstring}
+end
+
+local expected_form = {
+	name = "manager_spec",
+	options = {w = 5, h = 10},
+	elements = {
+		{
+			variations = {},
+			name = "anchor",
+			variation = {
+				name = "anchor",
+				cache = {
+					map_fields = {"x", "y"},
+				},
+				fields = {
+					{"x", "number", separator = ","},
+					{"y", "number"}
+				},
+				def = {x = 0, y = 0}
+			}
+		}
+	},
+	model = {}
+}
 
 describe("FormspecManager", function()
 	local instance = FormspecManager:new("unit_test")
 
-	it("collects formspec name, elements, and model for addition to the instance", function()
+	it("collects formspec name, options, elements, and model for addition to the instance", function()
 		assert.has_no.error(function()
 			instance("manager_spec") { w = 5, h = 10 } {
 				anchor { x = 0, y = 0 }
 			} {}
 		end)
+		assert.are.same(expected_form, instance.forms[1])
 	end)
 
-	it("renders a registered formspec, returning a Minetest-compatible formspec string", function()
-		assert.are.equal("real_coordinates[true]size[5,10]anchor[0,0]", instance:render("manager_spec"))
+	describe("get", function()
+		it("returns a formspec by name from the instance", function()
+			assert.are.same(expected_form, instance:get("manager_spec"))
+			assert.falsy(instance:get("invalid"))
+		end)
+	end)
+
+	describe("get_index", function()
+		it("returns a formspec index by name from the instance", function()
+			assert.are.equal(1, instance:get_index("manager_spec"))
+			assert.falsy(instance:get_index("invalid"))
+		end)
+	end)
+
+	describe("render", function()
+		it("returns a Minetest-compatible formspec string", function()
+			assert.are.equal("real_coordinates[true]size[5,10]anchor[0,0]", instance:render("manager_spec"))
+			assert.falsy(instance:render("invalid"))
+		end)
+	end)
+
+	describe("show", function()
+		it("shows a formspec to an in-game player", function()
+			assert.has_no.error(function() instance:show("manager_spec", "singleplayer") end)
+			assert.are.same({"singleplayer", "unit_test:manager_spec", "real_coordinates[true]size[5,10]anchor[0,0]"},
+				last_show_formspec_call)
+			assert.has_error(function() instance:show("invalid", "singleplayer") end,
+				"libuix().formspec['invalid']:show: formspec does not exist or contains no elements")
+		end)
 	end)
 end)
